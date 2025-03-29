dd3b4d25d2bca7464a4ba1bfa5e4be2b8bd2f3e3
Eugene <evgenii.esaulenko@gmail.com>
2025-03-29 13:23:57 +0100

    Fix Rect draw method to support rotation around center pivot

--- include/framework/GameObjectRect.h
+++ include/framework/GameObjectRect.h
@@ -11,7 +11,14 @@ public:
     GameObjectRect(const int width, const int height) : m_width(width), m_height(height) {}
     GameObjectRect(const Vector2& position, const Color& color, const int width, const int height) : GameObject2D(position, color), m_width(width), m_height(height) {}
 
-    void Draw() const override { DrawRectanglePro(GetRectangle(), {0.f, 0.f}, m_rotation, m_color); }
+    void Draw() const override
+    {
+        const float width = static_cast<float>(m_width);
+        const float height = static_cast<float>(m_height);
+        const Vector2 origin = {width / 2, height / 2};
+        const Rectangle viewRect = {m_position.x, m_position.y, width, height};
+        DrawRectanglePro(viewRect, origin, m_rotation, m_color);
+    }
 
     Rectangle GetRectangle() const
     {
